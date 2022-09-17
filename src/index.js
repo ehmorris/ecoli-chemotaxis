00e3687aa58a6158e52bc0136a48fef7b5c5e1bb
Edwin Morris <edwin.h.morris@gmail.com>
2022-09-16 22:02:16 -0400

    Remove clearcanvas comment

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const CTX = generateCanvas({
 });
 
 const drawFrame = () => {
-  //CTX.clearRect(0, 0, canvasProperties.width, canvasProperties.height);
+  CTX.clearRect(0, 0, canvasProperties.width, canvasProperties.height);
 
   // Draw background
   ecoliEntity.draw(CTX);
