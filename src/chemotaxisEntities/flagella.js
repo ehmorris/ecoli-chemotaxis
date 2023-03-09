91fef8177ea28c449eb046dd64a42f99d16dc233
Edwin <edwin.h.morris@gmail.com>
2023-03-08 20:26:54 -0500

    Revert "WIP tumble run attached to motors"
    This reverts commit 599c6617e49b00318f4d36671d2d28dddd29cce0.

--- src/chemotaxisEntities/flagella.js
+++ src/chemotaxisEntities/flagella.js
@@ -31,26 +31,26 @@ const transitionPathPair = (pathPair, progress, easing) => {
 };
 
 export const makeFlagella = (CTX, runData, tumbleData) => {
-  const _state = new Map()
+  const state = new Map()
     .set("activeSet", runData)
     .set("transitionPhase", 0)
     .set("transitionTargetSet", null);
 
   const tumble = () => {
     if (
-      _state.get("activeSet") !== tumbleData &&
-      _state.get("transitionPhase") === 0
+      state.get("activeSet") !== tumbleData &&
+      state.get("transitionPhase") === 0
     ) {
-      _state.set("transitionTargetSet", tumbleData).set("transitionPhase", 1);
+      state.set("transitionTargetSet", tumbleData).set("transitionPhase", 1);
     }
   };
 
   const run = () => {
     if (
-      _state.get("activeSet") !== runData &&
-      _state.get("transitionPhase") === 0
+      state.get("activeSet") !== runData &&
+      state.get("transitionPhase") === 0
     ) {
-      _state.set("transitionTargetSet", runData).set("transitionPhase", 1);
+      state.set("transitionTargetSet", runData).set("transitionPhase", 1);
     }
   };
 
@@ -58,58 +58,60 @@ export const makeFlagella = (CTX, runData, tumbleData) => {
     CTX.save();
     CTX.lineWidth = 10;
     CTX.lineCap = "round";
+    CTX.translate(-100, -300);
+    //CTX.rotate((-100 * Math.PI) / 180);
     const transitionDuration = 400;
 
-    if (_state.get("transitionPhase") === 1) {
+    if (state.get("transitionPhase") === 1) {
       // Create a new set that captures the current state of the active set as the
       // start of the animation. Set the end state to the beginning of the normal
       // loop that's targeted
       const newActiveSet = {
         from: transitionPathPair(
-          _state.get("activeSet"),
+          state.get("activeSet"),
           mirroredLoopingProgress(
             0,
-            _state.get("activeSet").animationDuration,
+            state.get("activeSet").animationDuration,
             millisecondsElapsed()
           ),
           easeInOutSine
         ),
         to: {
-          path: _state.get("transitionTargetSet").from.path,
+          path: state.get("transitionTargetSet").from.path,
           position: {
-            x: _state.get("transitionTargetSet").from.position.x,
-            y: _state.get("transitionTargetSet").from.position.y,
+            x: state.get("transitionTargetSet").from.position.x,
+            y: state.get("transitionTargetSet").from.position.y,
           },
         },
         animationDuration: transitionDuration,
-        lightness: _state.get("transitionTargetSet").lightness,
+        lightness: state.get("transitionTargetSet").lightness,
       };
 
-      _state.set("activeSet", newActiveSet).set("transitionPhase", 2);
+      state.set("activeSet", newActiveSet).set("transitionPhase", 2);
       resetElapsedTime();
     }
 
     if (
-      _state.get("transitionPhase") === 2 &&
+      state.get("transitionPhase") === 2 &&
       millisecondsElapsed() >= transitionDuration
     ) {
-      _state
-        .set("activeSet", _state.get("transitionTargetSet"))
+      state
+        .set("activeSet", state.get("transitionTargetSet"))
         .set("transitionPhase", 0);
       resetElapsedTime();
     }
 
     const pathAtPoint = transitionPathPair(
-      _state.get("activeSet"),
+      state.get("activeSet"),
       mirroredLoopingProgress(
         0,
-        _state.get("activeSet").animationDuration,
+        state.get("activeSet").animationDuration,
         millisecondsElapsed()
       ),
       easeInOutSine
     );
 
-    CTX.strokeStyle = `hsl(90, 18%, ${_state.get("activeSet").lightness}%)`;
+    CTX.strokeStyle = `hsl(90, 18%, ${state.get("activeSet").lightness}%)`;
     CTX.translate(pathAtPoint.position.x, pathAtPoint.position.y);
     CTX.stroke(new Path2D(pathAtPoint.path));
     CTX.restore();
