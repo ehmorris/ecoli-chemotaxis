efb104fd6140383764dee6094a0f69752be11e70
jsomers <jsomers@gmail.com>
2024-03-18 23:16:18 -0400

    interval = 13

--- ecoliSimulation/data.js
+++ ecoliSimulation/data.js
@@ -4,7 +4,7 @@ export const canvasProperties = {
   width,
   height: Math.min(Math.max(width * 0.4, 400), width),
   illustrationScale: Math.min(width / 1000, 1),
-  interval: 10,
+  interval: 13,
 };
 
 export const graphProperties = {
