ebcd54cbed5fee656b0ba7fd01720ad4792c296d
Edwin <edwin.h.morris@gmail.com>
2024-03-19 20:40:26 -0400

    Back to normal speed

--- ecoliSimulation/data.js
+++ ecoliSimulation/data.js
@@ -13,7 +13,7 @@ export const canvasProperties = {
     fullWidthContainerWidth
   ),
   illustrationScale: Math.min(fullWidthContainerWidth / 1000, 1),
-  interval: Math.floor(1000 / 10),
+  interval: Math.floor(1000 / 60),
 };
 
 export const graphProperties = {
